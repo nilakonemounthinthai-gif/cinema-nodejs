2eff0fcd9f92b3f69833802f1e7d492e7e1d50b7
Nilakone Mtt <nilakonemounthinthai@gmail.com>
2026-06-13 21:17:47 +0700

    Switch to mysql2 in backend configuration for MySQL 8+ compatibility

--- Binema/config/db.js
+++ Binema/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 // Use a connection POOL instead of a single createConnection.
 // With createConnection, if MySQL closes the idle connection (wait_timeout),
